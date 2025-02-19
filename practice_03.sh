d531829eef9c4962a12e5abc2160e321cc9ee67e
orawanawaro <rj.orawan2002@gmail.com>
2025-02-19 15:50:05 +0700

    Add files via upload

---
+++ practice_03.sh
@@ -0,0 +1,48 @@
+
+echo #1. fasta aln with fastq  sam
+#minimap2 -ax map-ont ref/sequence.fasta SRR16094814.fastq.gz > aln.sam
+
+echo #2. sort sam to bam
+#./program/samtools-1.9/samtools sort aln.sam -o aln.bam
+
+
+echo #3. creat bam index
+#./program/samtools-1.9/samtools index aln.bam
+
+echo #3.1 filter mapped read
+#./program/samtools-1.9/samtools view -b  -F 4 aln.bam > mapped.bam
+#./program/samtools-1.9/samtools index mapped.bam
+
+
+#4. igv input- ncbi fasta, bam output
+
+echo #5. convert bam to vcf
+#./program/bcftools-1.21/bcftools mpileup -Ov -f ref/sequence.fasta mapped.bam | ./program/bcftools-1.21/bcftools call -mv -o sample.vcf
+
+#6. funtion annotation
+
+#mkdir output
+
+for i in $( ls dataset/sample/*.gz);
+
+do
+    echo $i
+    fl=$(echo $i | cut -d"/" -f3 | cut -d"_" -f1)
+    #mkdir -p output/$fl
+   
+    #step 0 count read
+    seqkit stat $i > output/$fl/stat.txt 
+
+    #2. fasta aln with fastq  sam
+    minimap2 -ax map-ont ref/sequence.fasta $i > output/$fl/aln.sam
+
+    
+
+
+
+
+
+
+
+
+done 
